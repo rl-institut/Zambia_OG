fd66a8c08cfbc3af0b6f0ab43c62945648132ce0
Christian Hofmann <christian.hofmann@rl-institut.de>
2018-10-26 09:13:21 +0200

    added temporary dummy data

---
+++ data/centroids.js
@@ -0,0 +1,9 @@
+var centroids = {
+"type": "FeatureCollection",
+"name": "centroids",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": 0, "prio_class": 0, "building": 0, "area_km2": 0, "lat": 26.4919146, "lon": -16.5167092, "buid_dens": 0, "NAME_1": "Southern", "mobile_cov": null, "distance": 0, "numschool": 0 }, "geometry": { "type": "Point", "coordinates": [ 26.491914609882791, -16.516709225119801 ] } }
+
+]
+}
